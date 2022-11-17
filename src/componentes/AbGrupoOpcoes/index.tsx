417b742121787577879455083a349f3a6b7f9354
Margarida <margaridammcm@gmail.com>
2022-11-17 10:44:48 -0300

    estilo AbGrupoOpcoes

--- src/componentes/AbGrupoOpcoes/index.tsx
+++ src/componentes/AbGrupoOpcoes/index.tsx
@@ -1,8 +1,40 @@
 import React from "react";
+import styled from "styled-components";
+
+const SectionEstilizada = styled.section <{selecionado: boolean}>`
+    width: 194px;
+    height: 88px;
+    background: ${props => props.selecionado ? 'linear-gradient(97.54deg, #002F52 35.49%, #326589 165.37%)' : '#FFFFFF'};
+    border: 1px solid;
+    border-color: ${props => props.selecionado ? '002F52' : '#EB9B00'};
+    border-radius: 8px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    text-align: center;
+    margin: 10px;
+    font-family: sans-serif;
+    cursor: pointer;
+    header {
+        color: ${props => props.selecionado ? '#FFFFFF' : '#EB9B00'};
+        font-size: 12px;
+        font-weight: 400;
+    }
+    strong {
+        color: ${props => props.selecionado ? '#FFFFFF' : '#EB9B00'};
+        font-size: 16px;
+        font-weight: 700;
+    }
+    footer {
+        color: ${props => props.selecionado ? '#FFFFFF' : 'rgba(0, 0, 0, 54)'}; 
+        font-size: 12px;
+        font-weight: 400;
+    }
+`
 
 export const AbGrupoOpcoes = () => {
     return (<>
-        <section>
+        <SectionEstilizada selecionado={false}>
             <header>
                 E-book
             </header>
@@ -12,8 +44,8 @@ export const AbGrupoOpcoes = () => {
             <footer>
                 .pdf, .epub, .mob
             </footer>
-        </section>
-        <section>
+        </SectionEstilizada>
+        <SectionEstilizada selecionado={true}>
             <header>
                 E-book
             </header>
@@ -23,8 +55,8 @@ export const AbGrupoOpcoes = () => {
             <footer>
                 .pdf, .epub, .mob
             </footer>
-        </section>
-        <section>
+        </SectionEstilizada>
+        <SectionEstilizada selecionado={false}>
             <header>
                 E-book
             </header>
@@ -34,6 +66,6 @@ export const AbGrupoOpcoes = () => {
             <footer>
                 .pdf, .epub, .mob
             </footer>
-        </section>
+        </SectionEstilizada>
     </>)
 }

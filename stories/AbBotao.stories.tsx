ab17461fba6c3dbdfc526693f6e7a2f651208ca0
Margarida <margaridammcm@gmail.com>
2022-11-17 09:35:44 -0300

    evoluindo stories do AbBotao

--- stories/AbBotao.stories.tsx
+++ stories/AbBotao.stories.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AbBotao } from '../src/componentes/AbBotao'
+import { AbBotao, AbBotaoProps } from '../src/componentes/AbBotao'
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 
 export default {
@@ -7,6 +7,18 @@ export default {
     component: AbBotao
 } as ComponentMeta<typeof AbBotao>
 
-const Template: ComponentStory<typeof AbBotao> = () => <AbBotao />
+const Template: ComponentStory<typeof AbBotao> = (args) => <AbBotao {...args}/>
 
 export const Primario = Template.bind({})
+
+Primario.args = {
+    texto: 'Ab Botão Primário',
+    tipo: 'primario'
+} as AbBotaoProps
+
+export const Secundario = Template.bind({})
+
+Secundario.args = {
+    texto: 'Ab Botão Secundário',
+    tipo: 'secundario'
+} as AbBotaoProps
